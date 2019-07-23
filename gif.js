fd46173f422fc74fe807af87227ab7b27e3c9089
Phil Mauracher <gradientus@gmail.com>
2019-07-23 10:14:42 -0600

    Fixed a bug with double-rendering.  Everything is working, just need ability to click each gif animate/stop

--- gif.js
+++ gif.js
@@ -1,16 +1,21 @@
+//array of pre-set buttons
 var buttonArray = [
-  "Mercury",
-  "Venus",
-  "Earth",
-  "Mars",
-  "Jupiter",
-  "Saturn",
-  "Neptune",
-  "Uranus",
-  "Pluto",
-  "UFO"
+  "Rain",
+  "Snow",
+  "Tornado",
+  "Hurricane",
+  "Dust Devil",
+  "Water Spout",
+  "Sleet",
+  "Blizzard",
+  "Drought",
+  "Hail",
+  "Lightning",
+  "Sunny",
+  "Typhoon"
 ];
 
+//to make the topics render as buttons
 function renderbuttons() {
   $("#buttons").empty();
   for (var i = 0; i < buttonArray.length; i++) {
@@ -20,13 +25,9 @@ function renderbuttons() {
       }</button>`
     );
   }
-
-  $(document).on("click", ".topic", function() {
-    var index = $(this).attr("data-value");
-    displayImages(index);
-  });
 }
 
+//to fetch then display the images
 function displayImages(i) {
   var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=JzubCXWkSrfanIsUN38CVmpc6ihcHZiu&q=${
     buttonArray[i]
@@ -36,19 +37,22 @@ function displayImages(i) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    var animated = response.data[0].images.downsized_large.url;
-    var still = response.data[0].images.downsized_still.url;
-    var rating = response.data[0].rating;
-    $("#displayArea").append(`<img src="${animated}" class="images">`);
-
-    // loop the data
-    // loop what info I need from the object
-    // render images
-
-    // include you will need to url for theimages because on clinck you will need to be able to swtich
+    for (var i = 0; i < 10; i++) {
+      var animated = response.data[i].images.downsized_large.url;
+      var still = response.data[i].images.downsized_still.url;
+      var rating = response.data[i].rating;
+      $("#displayArea").append(`<img src="${animated}" class="images">`);
+    }
   });
 }
 
+//to be able to tell which button was click on
+$(document).on("click", ".topic", function() {
+  var index = $(this).attr("data-value");
+  displayImages(index);
+});
+
+//to allow the user to add topics
 $(document).on("click", "#addtopic", function(event) {
   event.preventDefault();
   var topicinfo = $("#topic")
@@ -59,4 +63,7 @@ $(document).on("click", "#addtopic", function(event) {
   renderbuttons();
 });
 
+//call to render the buttons (pre-made or otherwise)
 renderbuttons();
+
+//TODO: Add the ability to click a gif and have is become still or animated, opposite of whatever state it is in currently
